f5191c04e80f0426741aea7aee19d3af646d94ca
Maloto7 <agustin.colque@gmail.com>
2020-01-25 12:22:38 -0400

    add 10

---
+++ 04-Forms/formDemo.js
@@ -0,0 +1,20 @@
+describe('form angular demo', function() {
+    it('should works whit non angular page', function () {
+
+        browser.get('https://qaclickacademy.github.io/protocommerce/');
+
+
+        element(by.name("name")).sendKeys("Agustin");
+        element(by.css("input[name='email']")).sendKeys("agustin@mail.com");
+        element(by.id("exampleInputPassword1")).sendKeys("Password.1");
+        element(by.cssContainingText("[id='exampleFormControlSelect1'] option","Female")).click();
+        element.all(by.name("inlineRadioOptions")).first().click();
+        element(by.name("bday")).sendKeys("11112000");
+        element(by.buttonText("Submit")).click().then((text)=>{
+            element(by.css("div[class*='success']")).getText().then((text)=>{
+                // console.log(text);
+                expect(text).toContain("Success");
+            });
+        });
+    });
+});
